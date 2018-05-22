72a073ed51bba5e565e1f7da662703b6f241511a
Pascal Skupa <pascal.skupa@htl.rennweg.at>
2018-05-22 23:13:13 +0200

    First

---
+++ impressum.php
@@ -0,0 +1,53 @@
+Impressum
+Informationspflicht laut §5 E-Commerce Gesetz, §14 Unternehmensgesetzbuch, §63 Gewerbeordnung und Offenlegungspflicht laut §25 Mediengesetz.
+
+Pascal Skupa
+
+Gemeindeweg 4-8, Haus 9 ,
+2201 Gerasdorf,
+Österreich
+
+E-Mail: pascal.skupa@htl.rennweg.at
+
+
+EU-Streitschlichtung
+Angaben zur Online-Streitbeilegung: Verbraucher haben die Möglichkeit, Beschwerden an die OnlineStreitbeilegungsplattform der EU zu richten: https://ec.europa.eu/consumers/odr/main/index.cfm?event=main.home2.show&lng=DE. Sie können allfällige Beschwerde auch an die oben angegebene E-Mail-Adresse richten.
+
+Haftung für Inhalte dieser Webseite
+Wir entwickeln die Inhalte dieser Webseite ständig weiter und bemühen uns korrekte und aktuelle Informationen bereitzustellen. Leider können wir keine Haftung für die Korrektheit aller Inhalte auf dieser Webseite übernehmen, speziell für jene die seitens Dritter bereitgestellt wird. Sollten Ihnen problematische oder rechtswidrige Inhalte auffallen, bitte wir Sie uns umgehend zu kontaktieren, Sie finden die Kontaktdaten im Impressum.
+
+Haftung für Links auf dieser Webseite
+Unsere Webseite enthält Links zu anderen Webseiten für deren Inhalt wir nicht verantwortlich sind. Wenn Ihnen rechtswidrige Links auf unserer Webseite auffallen, bitte wir Sie uns zu kontakieren, Sie finden die Kontaktdaten im Impressum.
+
+Urheberrechtshinweis
+Alle Inhalte dieser Webseite (Bilder, Fotos, Texte, Videos) unterliegen dem Urheberrecht. Falls notwendig, werden wir die unerlaubte Nutzung von Teilen der Inhalte unserer Seite rechtlich verfolgen.
+
+Bildernachweis
+Die Bilder, Fotos und Grafiken auf dieser Webseite sind urheberrechtlich geschützt.
+
+Die Bilderrechte liegen bei den folgenden Fotografen und Unternehmen:
+
+...
+...
+
+Datenschutzerklärung
+Datenschutz
+Wir haben diese Datenschutzerklärung verfasst, um Ihnen gemäß der Vorgaben der EU-Datenschutz-Grundverordnung zu erklären, welche Informationen wir sammeln, wie wir Daten verwenden und welche Entscheidungsmöglichkeiten Sie als Besucher dieser Webseite haben.
+
+Leider liegt es in der Natur der Sache, dass diese Erklärungen sehr technisch klingen, wir haben uns bei der Erstellung jedoch bemüht die wichtigsten Dinge so einfach und klar zu beschreiben.
+
+Ihre Rechte
+Ihnen stehen grundsätzlich die Rechte auf Auskunft, Berichtigung, Löschung, Einschränkung, Datenübertragbarkeit, Widerruf und Widerspruch zu. Wenn Sie glauben, dass die Verarbeitung Ihrer Daten gegen das Datenschutzrecht verstößt oder Ihre datenschutzrechtlichen Ansprüche sonst in einer Weise verletzt worden sind, können Sie sich bei der Aufsichtsbehörde beschweren. In Österreich ist dies die Datenschutzbehörde, deren Webseiten Sie unter https://www.dsb.gv.at/ finden.
+
+Google Analytics Datenschutzerklärung
+Wir verwenden auf dieser Webseite Google Analytics der Firma Google Inc. (1600 Amphitheatre Parkway Mountain View, CA 94043, USA) um Besucherdaten statistisch auszuwerten. Dabei verwendet Google Analytics zielorientierte Cookies.
+
+Nähere Informationen zu Nutzungsbedingungen und Datenschutz finden Sie unter http://www.google.com/analytics/terms/de.html bzw. unter https://support.google.com/analytics/answer/6004245?hl=de.
+
+Pseudonymisierung
+Unser Anliegen im Sinne der DSGVO ist die Verbesserung unseres Angebotes und unseres Webauftritts. Da uns die Privatsphäre unserer Nutzer wichtig ist, werden die Nutzerdaten pseudonymisiert. Die Datenverarbeitung erfolgt auf Basis der gesetzlichen Bestimmungen des § 96 Abs 3 TKG sowie des Art 6 EU-DSGVO Abs 1 lit a (Einwilligung) und/oder f (berechtigtes Interesse) der DSGVO.
+
+Deaktivierung der Datenerfassung durch Google Analytics
+Mithilfe des Browser-Add-ons zur Deaktivierung von Google Analytics-JavaScript (ga.js, analytics.js, dc.js) können Website-Besucher verhindern, dass Google Analytics ihre Daten verwendet.
+
+Sie können die Erfassung der durch das Cookie erzeugten und auf Ihre Nutzung der Website bezogenen Daten an Google sowie die Verarbeitung dieser Daten durch Google verhindern, indem Sie das unter dem folgenden Link verfügbare Browser-Plugin herunterladen und installieren: https://tools.google.com/dlpage/gaoptout?hl=de.
---
+++ index.php
@@ -0,0 +1,303 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Storm Pi</title>
+
+    <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css">
+    <!--[if lte IE 8]>
+    <link rel="stylesheet" href="css/grid-old-ie.css">
+    <![endif]-->
+    <!--[if gt IE 8]><!-->
+    <link rel="stylesheet" href="css/grid.css">
+    <!--<![endif]-->
+    <!--[if lte IE 8]>
+    <link rel="stylesheet" href="css/layout/side-menu-old-ie.css">
+    <![endif]-->
+    <!--[if gt IE 8]><!-->
+    <link rel="stylesheet" href="css/layout/side-menu.css">
+    <!--<![endif]-->
+
+    <link rel="stylesheet"
+          href="https://unpkg.com/purecss-components@0.0.12/dist/pure-components.css"
+          integrity="sha384-3vxDvOU9lXU+bcgTkQNhnflfhRt/EFEGLtd3jQn8vQRGGQlpBX9VOq4oIufzLOO9"
+          crossorigin="anonymous">
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="css/css1.css">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery.simpleWeather/3.1.0/jquery.simpleWeather.min.js"></script>
+
+    <script src="//code.simplesvg.com/1/1.0.0-beta5/simple-svg.min.js"></script>
+
+    <script src="js/js1.js"></script>
+
+</head>
+
+<body>
+
+<div id="layout">
+    <!-- Menu toggle -->
+    <a href="#menu" id="menuLink" class="menu-link">
+        <!-- Hamburger icon -->
+        <span></span>
+    </a>
+
+    <div id="menu">
+        <div class="pure-menu">
+            <a class="pure-menu-heading" href="#readings">Storm Pi</a>
+
+            <ul class="pure-menu-list">
+                <li class="pure-menu-item">
+                    <a href="#readings" class="pure-menu-link">Readings</a>
+                </li>
+                <li class="pure-menu-item menu-item-divided">
+                    <a href="#sensorstatus" class="pure-menu-link">Sensoren</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <?php
+    $host = "localhost";
+    $user = "stormpi";
+    $pass = "anubis3!";
+    $db_name = "StormPi";
+
+    //create connection
+    $connection = mysqli_connect($host, $user, $pass, $db_name);
+
+    //test if connection failed
+    if (mysqli_connect_errno()) {
+        die("connection failed: "
+            . mysqli_connect_error()
+            . " (" . mysqli_connect_errno()
+            . ")");
+    }
+
+    //get results from database
+    $result = mysqli_query($connection, "SELECT ID, Humidity, Temperature, Windchill, Pressure, DateTime, Altitude  from measuring_result ORDER BY DateTime DESC LIMIT 1;");
+    $all_property = array();  //declare an array for saving property
+
+    $row = mysqli_fetch_array($result);
+    $humidity = $row['Humidity'];
+    $temp = $row['Temperature'];
+    $press = $row['Pressure'];
+    $id = $row['ID'];
+    $windchill = $row['Windchill'];
+    $time = $row['DateTime'];
+    $alt = $row['Altitude'];
+    //showing property
+    ?>
+    <div id="main">
+        <div class="header">
+            <h1>Storm Pi</h1>
+            <h2>An all in One weather solution</h2>
+        </div>
+
+        <div class="pure-g weathercard">
+            <div class="pure-u-1 weatherbox1">
+
+                <div class="row">
+                    <h3 id="location">Location</h3>
+                    <span class="simple-svg icon:raphael-location icon-inline:false">::before</span>
+                </div>
+            </div>
+
+            <div class="pure-u-2-3 weatherbox2">
+                <div class="row">
+                    <h1 id="temp"><?php echo $temp . " "; ?></h1>
+                    <span class="simple-svg icon:wi-celsius icon-inline:false">::before</span>
+                    <span class="simple-svg icon:wi-thermometer icon-inline:false">::before</span>
+                </div>
+            </div>
+            <div class="pure-u-1-3 weatherbox3">
+                <div class="row">
+                    <div id='weather-icon'></div>
+                    <p id="weather-type">Weather type</p>
+                </div>
+                <div class="row">
+                    <p id="humidity"><?php echo $humidity . " "; ?></p>
+                    <span class="simple-svg icon:wi-humidity icon-inline:false">::before</span>
+                </div>
+                <div class="row">
+                    <p id="pressure"><?php echo $press . " "; ?></p>
+                    <span class="simple-svg icon:wi-barometer icon-inline:false">::before</span>
+                </div>
+            </div>
+        </div>
+        <hr hidden id="readings">
+        <div class="content">
+            <div class="chart1">
+                <canvas id="chart1">
+                    <script>
+                        var ctx = document.getElementById("chart1").getContext('2d');
+                        var myLineChart = new Chart(ctx, {
+                            type: 'line',
+                            data: {
+                                fill: false
+                            },
+                            options: {
+                                scales: {
+                                    xAxes: [{
+                                        time: {
+                                            unit: 'hour'
+                                        }
+                                    }]
+                                }
+                            }
+                        });
+                    </script>
+                </canvas>
+            </div>
+
+            <h1 class="content-subhead">Readings</h1>
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip
+                ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                occaecat
+                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <div class="pure-g">
+                <div class="pure-u-1 pure-u-md-1-3">
+                    <h2 class="content-subhead"> Humidity </h2>
+                    <span class="displaynumber"> <?php echo $humidity; ?> </span>
+                </div>
+                <div class="pure-u-1 pure-u-md-1-3">
+                    <h2 class="content-subhead"> Temperature </h2>
+                    <span class="displaynumber"> <?php echo $temp; ?> </span>
+                </div>
+                <div class="pure-u-1 pure-u-md-1-3">
+                    <h2 class="content-subhead"> Pressure </h2>
+                    <span class="displaynumber"> <?php echo $press; ?>
+                </div>
+            </div>
+        </div>
+        <hr id="sensorstatus">
+        <div class="content">
+            <h1 class="content-subhead">Sensoren</h1>
+            <p>
+                Aktuell verbaute Sensoren
+            </p>
+            <h2 class="content-subhead">DHT-22</h2>
+            <div class="pure-g">
+                <div class="status pure-u-1-8"></div>
+                <div class="pure-u-1-6">ACTIVE</div>
+            </div>
+            <p>
+                DHT22 output calibrated digital signal. It utilizes exclusive digital-signal-collecting-technique
+                and
+                humidity
+                sensing technology, assuring its reliability and stability.Its sensing elements is connected with
+                8-bit
+                single-chip
+                computer.
+                Every sensor of this model is temperature compensated and calibrated in accurate calibration chamber
+                and
+                the
+                calibration-coefficient is saved in type of programme in OTP memory, when the sensor is detecting,
+                it
+                will cite
+                coefficient from memory.
+                Small size & low consumption & long transmission distance(20m) enable DHT22 to be suited in all
+                kinds of
+                harsh application occasions.
+                Single-row packaged with four pins, making the connection very convenient
+            </p>
+            <h2 class="content-subhead">TSL2561</h2>
+            <div class="pure-g">
+                <div class="status pure-u-1-8"></div>
+                <div class="pure-u-1-6">ACTIVE</div>
+            </div>
+            <p>
+                The TSL2561 luminosity sensor is an advanced digital light sensor, ideal for use in a wide range of
+                light situations. Compared to low cost CdS cells, this sensor is more precise, allowing for exact
+                lux
+                calculations and can be configured for different gain/timing ranges to detect light ranges from up
+                to
+                0.1 - 40,000+ Lux on the fly. The best part of this sensor is that it contains both infrared and
+                full
+                spectrum diodes! That means you can separately measure infrared, full-spectrum or human-visible
+                light.
+                Most sensors can only detect one or the other, which does not accurately represent what human eyes
+                see
+                (since we cannot perceive the IR light that is detected by most photo diodes)
+            </p>
+            <h2 class="content-subhead">BMP180</h2>
+            <div class="pure-g">
+                <div class="status pure-u-1-8"></div>
+                <div class="pure-u-1-6">ACTIVE</div>
+            </div>
+            <p>
+                This precision sensor from Bosch is the best low-cost sensing solution for measuring barometric
+                pressure
+                and temperature. Because pressure changes with altitude you can also use it as an altimeter! The
+                sensor
+                is soldered onto a PCB with a 3.3V regulator, I2C level shifter and pull-up resistors on the I2C
+                pins.
+
+                The BMP180 is the next-generation of sensors from Bosch, and replaces the BMP085. The good news is
+                that
+                it is completely identical to the BMP085 in terms of firmware/software - you can use our BMP085
+                tutorial
+                and any example code/libraries as a drop-in replacement. The XCLR pin is not physically present on
+                the
+                BMP180 so if you need to know that data is ready you will need to query the I2C bus.
+            </p>
+            <h2 class="content-subhead">DS18B20</h2>
+            <div class="pure-g">
+                <div class="status pure-u-1-8"></div>
+                <div class="pure-u-1-6">ACTIVE</div>
+            </div>
+            <p>
+                This is the latest DS18B20 1-Wire digital temperature sensor from Maxim IC. Reports degrees C with 9
+                to
+                12-bit precision, -55C to 125C (+/-0.5C). Each sensor has a unique 64-Bit'serial number etched into
+                it -
+                allows for a huge number of sensors to be used on one data bus. This is a wonderful part that is the
+                corner stone of many data-logging and temperature control projects.
+            </p>
+        </div>
+        <hr>
+        <div class="content">
+            <div class="footer">
+                <div class="pure-g">
+                    <div class="pure-u-1">
+                        <div class="pure-g">
+                            <div class="pure-u-1 pure-u-md-1-2">
+                                <a href="#" class="button footerbutton githubButton">Github</a>
+                            </div>
+                            <div class="pure-u-1 pure-u-md-1-2">
+                                <a href="#" class="button footerbutton emailButton">Contact</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pure-u-1">
+                        <p class="legal-copyright">
+                            © 2018 - Present Project Storm Pi All rights reserved.
+                        </p>
+                        <a href="impressum.php" style="color:inherit">Legal</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <script src="js/ui.js"></script>
+
+</body>
+
+</html>
